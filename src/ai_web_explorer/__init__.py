2a336434fcdef268f60677daf395080a4af55ec2
Milos Svana <milos.svana@gmail.com>
2024-03-25 09:48:34 +0100

    Implement opening a webpage

--- src/ai_web_explorer/__init__.py
+++ src/ai_web_explorer/__init__.py
@@ -1,5 +1,9 @@
 import argparse
+import logging
+import playwright.sync_api
 
+
+logging.basicConfig(level=logging.INFO)
 
 parser = argparse.ArgumentParser(description="Explore a website")
 parser.add_argument("domain", help="The domain to start the exploration from")
@@ -7,5 +11,17 @@ parser.add_argument("domain", help="The domain to start the exploration from")
 
 def main():
     args = parser.parse_args()
-    print(args)
+    domain = args.domain
+    logging.info(f"Exploring {domain}")
+    
+    pw = playwright.sync_api.sync_playwright().start()
+    browser = pw.chromium.launch()
+    page = browser.new_page()
+    page.goto(domain)
 
+    
+
+
+    page.close()
+    browser.close()
+    pw.stop()
