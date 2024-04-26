125ca585984fb3b277772f8e687c852b69214442
Milos Svana <milos.svana@gmail.com>
2024-04-26 13:41:19 +0200

    Add Claude

--- src/ai_web_explorer/promptrepo.py
+++ src/ai_web_explorer/promptrepo.py
@@ -1,8 +1,10 @@
 import dataclasses
 
+import anthropic
 import openai
 from openai.types import chat, shared_params
 import yaml
+import copy
 
 from . import config
 
@@ -48,6 +50,24 @@ class Prompt:
             tool_choice=tool_choice,
         )
         return completion
+
+    def execute_prompt_anthropic(self, model: str, **data):
+        prompt_text = self.prompt_with_data(**data)
+        
+        tools = copy.deepcopy(self.functions)
+        for tool in tools:
+            tool["input_schema"] = tool["parameters"]
+            del tool["parameters"]
+
+        response = anthropic.Anthropic().beta.tools.messages.create(
+            model=model,
+            messages=[{"role": "user", "content": prompt_text}],
+            temperature=self.temperature,
+            max_tokens=2048,
+            tools=tools,
+        )
+
+        return response
 
 def get_prompt(name: str) -> Prompt:
     with open(config.PROMPTS_PATH) as f:
--- tests/ai/test_title.py
+++ tests/ai/test_title.py
@@ -1,6 +1,7 @@
 import json
 import os
 import datetime
+from anthropic.types.beta.tools import ToolUseBlock
 import mlflow
 import numpy as np
 import openai
@@ -17,6 +18,10 @@ PRICES = {
     "gpt-3.5": {
         "input": 0.5,
         "output": 1.5,
+    },
+    "claude-3-sonnet": {
+        "input": 3.0,
+        "output": 15.0,
     },
 }
 
@@ -68,42 +73,61 @@ def test_title():
         with open(os.path.join(DATA_DIR, filename)) as f:
             html = f.read()[: config.HTML_PART_LENGTH]
             time_start = datetime.datetime.now()
-            completion = prompt.execute_prompt(openai.OpenAI(), html=html)
-            response = completion.choices[0]
+            # completion = prompt.execute_prompt(html=html)
+            # response = completion.choices[0]
+            completion = prompt.execute_prompt_anthropic(
+                model="claude-3-sonnet-20240229", html=html
+            )
+            print(completion)
             time_end = datetime.datetime.now()
             eval_table["time"].append((time_end - time_start).total_seconds())
 
-        model_type = prompt.model.split("-")[0] + "-" + prompt.model.split("-")[1]
+        # model_type = prompt.model.split("-")[0] + "-" + prompt.model.split("-")[1]
+        model_type = "claude-3-sonnet"
 
         if model_type in PRICES and completion.usage:
             prices = PRICES[model_type]
-            price_input = prices["input"] * completion.usage.prompt_tokens / 1_000_000
-            price_output = (
-                prices["output"] * completion.usage.completion_tokens / 1_000_000
-            )
+            price_input = prices["input"] * completion.usage.input_tokens / 1_000_000
+            price_output = prices["output"] * completion.usage.output_tokens / 1_000_000
             price_total = price_input + price_output
         else:
             price_total = None
 
-        if not response.message.tool_calls or len(response.message.tool_calls) == 0:
-            raise ValueError("No tool calls in response when getting page title")
+        # if not response.message.tool_calls or len(response.message.tool_calls) == 0:
+        #    raise ValueError("No tool calls in response when getting page title")
 
-        args_str = response.message.tool_calls[0].function.arguments
-        args = json.loads(args_str)
-        title_generated = args["title"]
-        similarity = compare_embeddings(titles_expected[no - 1], title_generated)
+        # args_str = response.message.tool_calls[0].function.arguments
+        # args = json.loads(args_str)
+        # title_generated = args["title"]
+
+        for response in completion.content:
+            if type(response) == ToolUseBlock:
+                title_generated = response.input["title"]
+                similarity = compare_embeddings(titles_expected[no - 1], title_generated)
+                break
+        else:
+            title_generated = ""
+            similarity = 0
+
         eval_table["no"].append(no)
         eval_table["title_expected"].append(titles_expected[no - 1])
         eval_table["title_generated"].append(title_generated)
         eval_table["similarity"].append(similarity)
         eval_table["price"].append(price_total)
+        print(eval_table)
 
     with mlflow.start_run():
         mlflow.log_metric("similarity_mean", float(np.mean(eval_table["similarity"])))
         mlflow.log_metric("similarity_std", float(np.std(eval_table["similarity"])))
         mlflow.log_metric("time_mean", float(np.mean(eval_table["time"])))
         mlflow.log_metric("price_mean", float(np.mean(eval_table["price"])))
-        mlflow.log_metric("similarity_per_dollar", float(np.mean(eval_table["similarity"]) / np.mean(eval_table["price"])))
-        mlflow.log_metric("similarity_per_second", float(np.mean(eval_table["similarity"]) / np.mean(eval_table["time"])))
-        mlflow.log_param("model", prompt.model)
+        mlflow.log_metric(
+            "similarity_per_dollar",
+            float(np.mean(eval_table["similarity"]) / np.mean(eval_table["price"])),
+        )
+        mlflow.log_metric(
+            "similarity_per_second",
+            float(np.mean(eval_table["similarity"]) / np.mean(eval_table["time"])),
+        )
+        mlflow.log_param("model", model_type)
         mlflow.log_table(data=eval_table, artifact_file="eval_table.json")
